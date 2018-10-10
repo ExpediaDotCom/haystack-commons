29f84d7130a27e4d3bf1df8d112c1e1ef93f1910
Adrian Cole <acole@pivotal.io>
2018-10-10 11:38:10 +0800

    Updates travis config for maven wrapper

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -26,12 +26,12 @@ fi
 if [ ! -z "${TRAVIS_TAG}" ]
 then
     echo "travis tag is set -> updating pom.xml <version> attribute to ${TRAVIS_TAG}"
-    mvn --settings .travis/settings.xml org.codehaus.mojo:versions-maven-plugin:2.1:set -DnewVersion=${TRAVIS_TAG} 1>/dev/null 2>/dev/null
+    ./mvnw --settings .travis/settings.xml org.codehaus.mojo:versions-maven-plugin:2.1:set -DnewVersion=${TRAVIS_TAG} 1>/dev/null 2>/dev/null
 else
     echo "no travis tag is set, hence keeping the snapshot version in pom.xml"
 fi
 
-mvn clean deploy --settings .travis/settings.xml -B -U
+./mvnw clean deploy --settings .travis/settings.xml -B -U
 SUCCESS=$?
 
 if [ ${SUCCESS} -eq 0 ]
